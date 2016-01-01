0df342635d018a169d556fc6c19209e8f102f982
reubenson <reubenson@gmail.com>
2016-01-01 11:58:11 -0500

    need to debug

---
+++ app/assets/javascripts/gmap_styles.js
@@ -0,0 +1,78 @@
+var styles = [
+  {
+    stylers: [
+      // { hue: "#000000" },
+      { saturation: -10}
+    ]
+  },{
+    featureType: "road",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+var styles_2 = [
+  {
+    "featureType": "landscape",
+    "stylers": [
+        { "saturation": -100 },
+        { "lightness": 65 },
+        { "visibility": "on" }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+        { "saturation": -100 },
+        { "lightness": 51 },
+        { "visibility": "simplified" }
+    ]
+  },{
+    "featureType": "road.highway",
+    "stylers": [
+        { "saturation": -100 },
+        { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road.arterial",
+    "stylers": [
+        { "saturation": -100 },
+        { "lightness": 30 },
+        { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road.local",
+    "stylers": [
+        { "saturation": -100 },
+        { "lightness": 40 },
+        { "visibility": "on" }
+    ]
+  },{
+    "featureType": "transit",
+    "stylers": [
+        { "saturation": -100 },
+        { "visibility": "off" }
+    ]
+  },{
+    "featureType": "administrative.province",
+    "stylers": [
+        { "visibility": "off" }
+    ]
+  },{
+    "featureType": "water",
+    "elementType": "labels",
+    "stylers": [
+        { "visibility": "on" },
+        { "lightness": -25 },
+        { "saturation": -100 }
+    ]
+  },{
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+        { "hue": "#FEF8B8" },
+        { "lightness": -25 },
+        { "saturation": -97 }
+    ]
+  }
+]
